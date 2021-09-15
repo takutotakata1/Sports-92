8f49b97fb7be9d6bddb173dd9100f05bce29aee1
motty0533 <soutaro.ym@gmail.com>
2021-09-15 12:48:25 +0900

    jsonファイル

--- scripts/operate.js
+++ scripts/operate.js
@@ -1,37 +1,29 @@
+$.getJSON("/scripts/points.json")
+  .done(function (json) {
+    console.log(json.pointslist);
+    jQuery(function ($) {
 
-jQuery(function($){
-  $(".button1").on("click", function () {
-    var scores = [];
-    $(".point input").each(function(){
-      scores.push($(this).val());
+      $(".button1").on("click", function () {
+        var scores = [];
+        $(".point input").each(function () {
+          scores.push($(this).val());
+        });
+        for (let i = 0; i < scores.length; i++) {
+          console.log(scores[i] + "\n");
+          if (scores[i] != 0) {
+            json.pointslist[i] = scores[i]
+          }
+        }
+        alert("変更完了");
+        console.log(json.pointslist);
+      });
     });
-    for (let i = 0; i < scores.length; i++) {
-      console.log(scores[i]+"\n");
-    }
-    var outs = 0;
-    var onesum = 0;
-    var twosum = 0;
-    var threesum = 0;
-    var foursum = 0;
-    $(".ouputs").each(function(){
-      if(scores[outs]!=0){
-        $(this).text(scores[outs]);
-      }
-      // if(outs>3){
-      //   var num = Number(scores[outs]);
-      //   if(outs%4==0){
-      //     onesum += num;
-      //   } else if(outs%4==1){
-      //     twosum += num;
-      //   } else if(outs%4==2){
-      //     threesum += num; 
-      //   } else if(outs%4==3){
-      //     foursum += num;
-      //   }
-      // }
-      outs+=1;
+
+    $(document).ready(function () {
+      var outs = 0;
+      $(".outputs").each(function(){
+        $(this).text(json.pointslist[outs]);
+        outs += 1;
+      });
     });
-    alert("変更完了");
-    $(".midashi").text("12");
   });
-});
