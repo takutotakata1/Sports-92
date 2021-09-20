cbeda386802d8a914604ba731431c0aa391236f6
motty0533 <soutaro.ym@gmail.com>
2021-09-20 20:10:08 +0900

    学校アカウントか判定まで実装

--- backend/request.php
+++ backend/request.php
@@ -1,6 +1,11 @@
 <?php 
 header('Content-type: application/json; charset=utf-8');
 $data = filter_input(INPUT_POST,'gmail');
-$param = $data;
+$array = preg_split("/@/",$data);
+if($array[1]=='stg.nada.ac.jp'){
+  $param = true;
+} else{
+  $param = false;
+}
 echo json_encode($param);
  ?>
